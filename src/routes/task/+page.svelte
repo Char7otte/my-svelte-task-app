0d2c5836102a8855481e680c25acecba975a2184
Char7otte <faethychan@gmail.com>
2026-05-05 22:54:43 +0800

    feat: enhance tasks page

--- src/routes/task/+page.svelte
+++ src/routes/task/+page.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
 	import { enhance } from '$app/forms';
+	import RequiredAsterisk from '$lib/requiredAsterisk.svelte';
 	import type { PageData } from './$types';
 
 	const { data }: { data: PageData } = $props();
@@ -12,33 +13,38 @@
 	}
 </script>
 
-<div class="m-10">
-	<form method="POST" class="mb-10" use:enhance action="?/insert">
-		<h2>Create a task</h2>
-		<label for="title">Title:</label>
-		<input type="text" name="title" id="title" bind:value={titleInput} />
-		<label for="body">Body:</label>
-		<input type="text" name="body" id="body" bind:value={bodyInput} />
-		<button type="button" onclick={handleClearInputs}>Clear</button>
-		<button type="submit">Submit</button>
-	</form>
+<form method="POST" class="mb-10" use:enhance action="?/insert">
+	<h1 class="text-2xl">Create a task</h1>
+	<label for="title" class="block">Title: <RequiredAsterisk /></label>
+	<input type="text" name="title" id="title" bind:value={titleInput} required />
+	<label for="body" class="block">Body: <RequiredAsterisk /></label>
+	<textarea
+		name="body"
+		id="body"
+		bind:value={bodyInput}
+		class="mb-3 block border-2 border-solid"
+		required
+	>
+	</textarea>
+	<button type="submit">Submit</button>
+	<button type="button" onclick={handleClearInputs}>Clear</button>
+</form>
 
-	<ul>
-		{#if data.tasks.length === 0}
-			<li>There are no tasks. Create one above!</li>
-		{:else}
-			{#each data.tasks as { id, title, body, createdAt } (id)}
-				<li class="my-3">
-					<h3>{title}</h3>
-					<p>{body}</p>
-					<p>{createdAt.toString()}</p>
-					<form method="POST" action="?/delete" use:enhance>
-						<input type="hidden" name="id" value={id} />
-						<button type="submit">Delete</button>
-					</form>
-				</li>
-				<hr />
-			{/each}
-		{/if}
-	</ul>
-</div>
+<ul>
+	{#if data.tasks.length === 0}
+		<li>There are no tasks. Create one above!</li>
+	{:else}
+		{#each data.tasks as { id, title, body, createdAt } (id)}
+			<li class="my-3">
+				<h3>{title}</h3>
+				<p>{body}</p>
+				<p>{createdAt.toString()}</p>
+				<form method="POST" action="?/delete" use:enhance>
+					<input type="hidden" name="id" value={id} />
+					<button type="submit">Delete</button>
+				</form>
+			</li>
+			<hr />
+		{/each}
+	{/if}
+</ul>
