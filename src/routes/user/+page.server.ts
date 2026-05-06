7cdb26cb31d58a80ae5e5f594a41444ab5c0b3ef
shar <faethychan@gmail.com>
2026-05-06 12:15:42 +0800

    feat: implement sign in, set cookies, and redirect

--- src/routes/user/+page.server.ts
+++ src/routes/user/+page.server.ts
@@ -1,25 +1,33 @@
-import { postUser } from '$lib/server/database/users';
-import { createSession } from '$lib/server/sessionLib';
+import { comparePasswordHash } from '$lib/hashUtils.js';
+import { getUserByEmail, postUser } from '$lib/server/database/users';
+import { createSession } from '$lib/sessionLib.js';
 import type { User } from '$lib/types.js';
+import { error, redirect } from '@sveltejs/kit';
 import bcrypt from 'bcrypt';
-import { error } from 'console';
 
 export const actions = {
-	default: async ({ request }) => {
+	default: async ({ request, cookies }) => {
 		const data: FormData = await request.formData();
+
 		const email: string = data.get('email') as string;
 		const password: string = data.get('password') as string;
 		const option: string = data.get('option') as string;
 
 		if (option === 'sign in') {
-			// const user = await getUserByEmail(email);
-			// const isCorrect = await bcrypt.compare(password, user.passwordHash);
+			const user: User = await getUserByEmail(email);
+			if (!user) error(404, { message: 'Incorrect credentials' });
+			const isSamePassword = await comparePasswordHash(password, user.passwordHash);
+			if (!isSamePassword) error(404, { message: 'Incorrect credentials' });
+			const session = await createSession(user.id);
+			cookies.set('token', session.token, { path: '/' });
+			throw redirect(303, '/task');
 		} else if (option === 'sign up') {
 			const username = data.get('username') as string;
 			const passwordHash: string = await bcrypt.hash(password, 10);
 			const user: User = await postUser(email, passwordHash, username);
 			const session = await createSession(user.id);
-			console.log('Created Session: ', session);
+			cookies.set('token', session.token, { path: '/' });
+			throw redirect(303, '/task');
 		} else {
 			error(403, { message: "smh don't modify the form options" });
 		}
