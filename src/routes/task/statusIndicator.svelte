e2f10de9274626b5460ae81bfc40a7278d4dc071
shar <faethychan@gmail.com>
2026-05-06 17:44:14 +0800

    refactor: clean up statusIndicator

--- src/routes/task/statusIndicator.svelte
+++ src/routes/task/statusIndicator.svelte
@@ -1,13 +1,17 @@
 <script lang="ts">
 	let { status }: { status: 'not started' | 'working' | 'completed' } = $props();
+	let color: 'bg-green-600' | 'bg-orange-600' | 'bg-gray-600' | null = $derived.by(() => {
+		switch (status) {
+			case 'not started':
+				return 'bg-green-600';
+			case 'working':
+				return 'bg-orange-600';
+			case 'completed':
+				return 'bg-gray-600';
+			default:
+				return null;
+		}
+	});
 </script>
 
-<span>
-	{#if status === 'not started'}
-		<span class="mb-0.5 inline-block h-2 w-2 rounded-full bg-green-600"></span>
-	{:else if status === 'working'}
-		<span class="mb-0.5 inline-block h-2 w-2 rounded-full bg-orange-600"></span>
-	{:else}
-		<span class="mb-0.5 inline-block h-2 w-2 rounded-full bg-gray-600"></span>
-	{/if}
-</span>
+<span class="mb-0.5 inline-block h-2 w-2 rounded-full {color}"></span>
