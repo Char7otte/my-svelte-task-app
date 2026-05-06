5bd8dba01d56d049a944396c7fb2695bf90a6405
shar <faethychan@gmail.com>
2026-05-06 17:55:53 +0800

    refactor: use dictionary to get statusIndicator color

--- src/routes/task/statusIndicator.svelte
+++ src/routes/task/statusIndicator.svelte
@@ -1,17 +1,11 @@
 <script lang="ts">
 	let { status }: { status: 'not started' | 'working' | 'completed' } = $props();
-	let color: 'bg-green-600' | 'bg-orange-600' | 'bg-gray-600' | null = $derived.by(() => {
-		switch (status) {
-			case 'not started':
-				return 'bg-green-600';
-			case 'working':
-				return 'bg-orange-600';
-			case 'completed':
-				return 'bg-gray-600';
-			default:
-				return null;
-		}
-	});
+
+	const colorDictionary = {
+		'not started': 'bg-green-600',
+		working: 'bg-orange-600',
+		completed: 'bg-gray-600'
+	};
 </script>
 
-<span class="mb-0.5 inline-block h-2 w-2 rounded-full {color}"></span>
+<span class="mb-0.5 inline-block h-2 w-2 rounded-full {colorDictionary[status]}"></span>
