a6a8678f5fcc7143d95742584a914faf9a8975e3
shar <faethychan@gmail.com>
2026-05-06 15:21:21 +0800

    feat: enhance tasks page

--- src/routes/task/+page.svelte
+++ src/routes/task/+page.svelte
@@ -2,6 +2,7 @@
 	import { enhance } from '$app/forms';
 	import { resolve } from '$app/paths';
 	import RequiredAsterisk from '$lib/requiredAsterisk.svelte';
+	import { capitalize } from '$lib/utils/stringUtils';
 	import type { PageData } from './$types';
 
 	const { data }: { data: PageData } = $props();
@@ -16,14 +17,15 @@
 	}
 </script>
 
-{#if data.user}
-	<div>
-		<h1>{data.user.email}</h1>
-		<h1>{data.user.username}</h1>
-		<h1>{data.user.passwordHash}</h1>
-		<h1>{data.user.role}</h1>
-	</div>
-{/if}
+<div>
+	{#if disabled}
+		<h1 class="text-2xl">
+			You need to <a href={resolve('/user')} class="text-blue-600 underline">sign in</a> to create tasks.
+		</h1>
+	{:else}
+		<h1>Welcome, {data.user.username ? data.user.username : data.user.email}</h1>
+	{/if}
+</div>
 
 {#if disabled}
 	<div>
@@ -35,20 +37,14 @@
 
 <form method="POST" class="mb-10" use:enhance action="?/insert">
 	<fieldset {disabled}>
-		<h1 class="text-2xl">Create a task</h1>
+		<h2 class="text-2xl">Create a task</h2>
 		<label for="title" class="block">Title: <RequiredAsterisk /></label>
 		<input type="text" name="title" id="title" bind:value={titleInput} required />
-		<label for="body" class="block">Body: <RequiredAsterisk /></label>
-		<textarea
-			name="body"
-			id="body"
-			bind:value={bodyInput}
-			class="mb-3 block border-2 border-solid"
-			required
-		>
+		<label for="body" class="block">Body:</label>
+		<textarea name="body" id="body" bind:value={bodyInput} class="mb-3 block border-2 border-solid">
 		</textarea>
-		<button type="submit">Submit</button>
-		<button type="button" onclick={handleClearInputs}>Clear</button>
+		<button type="submit" class="primary">Submit</button>
+		<button type="button" onclick={handleClearInputs} class="secondary">Clear</button>
 	</fieldset>
 </form>
 
@@ -56,15 +52,19 @@
 	{#if data.tasks.length === 0}
 		<li>There are no tasks. Create one above!</li>
 	{:else}
-		{#each data.tasks as { id, title, body, createdAt } (id)}
+		{#each data.tasks as { id, title, body, createdAt, status, username, email } (id)}
 			<li class="my-3">
-				<h3>{title}</h3>
+				<div class="flex justify-between">
+					<div>
+						<h3 class="text-xl">{title}</h3>
+						<p class="text-[12px] text-gray-600">{username ? username : email}</p>
+					</div>
+					<div class="flex flex-col items-end">
+						<span>{capitalize(status)}</span>
+						<span class="text-[10px] text-gray-600">{createdAt.toLocaleString()}</span>
+					</div>
+				</div>
 				<p>{body}</p>
-				<p>{createdAt.toString()}</p>
-				<form method="POST" action="?/delete" use:enhance>
-					<input type="hidden" name="id" value={id} />
-					<button type="submit">Delete</button>
-				</form>
 			</li>
 			<hr />
 		{/each}
