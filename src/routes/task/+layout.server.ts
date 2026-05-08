377d237bd56b34a5c98f93f8bf7cb44e3a870b58
shar <faethychan@gmail.com>
2026-05-08 12:17:58 +0800

    refactor: define user explicitly

--- src/routes/task/+layout.server.ts
+++ src/routes/task/+layout.server.ts
@@ -1,11 +1,12 @@
 import { getTasksJoinUser } from '$lib/server/database/tasks';
-import type { TaskWithUser } from '$lib/types';
+import type { TaskWithUser, User } from '$lib/types';
 
 export async function load({ locals }) {
 	const tasks: TaskWithUser[] = await getTasksJoinUser();
+	const user: User = locals.user;
 
 	return {
 		tasks,
-		user: locals.user
+		user
 	};
 }
