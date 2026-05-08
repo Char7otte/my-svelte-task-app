c83d7490ad07cde829598dd4a83b6acd6a2b6964
shar <faethychan@gmail.com>
2026-05-08 12:21:29 +0800

    feat: create task slug

--- src/routes/task/[slug]/+page.svelte
+++ src/routes/task/[slug]/+page.svelte
@@ -1,2 +1,11 @@
-<label for="title">Title: </label>
-<input type="text" name="title" id="title">
+<script lang="ts">
+	import Task from '../task.svelte';
+	import type { PageData, RouteParams } from './$types';
+
+	let { data, params }: { data: PageData; params: RouteParams } = $props();
+	// Route ensures slugTask exists
+	let slugTask = $derived.by(() => data.tasks.find((task) => task.id === params.slug)!);
+</script>
+
+<h1>{params.slug}</h1>
+<Task {...slugTask} />
