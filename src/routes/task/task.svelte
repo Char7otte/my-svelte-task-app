52385297cb7a997b4c7e43ccccd5aa46cb704677
shar <faethychan@gmail.com>
2026-05-08 12:21:09 +0800

    feat: link task to slug

--- src/routes/task/task.svelte
+++ src/routes/task/task.svelte
@@ -1,16 +1,21 @@
 <script lang="ts">
+	import { resolve } from '$app/paths';
 	import { slide } from 'svelte/transition';
 	import StatusIndicator from './statusIndicator.svelte';
-	let { title, body, username, email, createdAt, status } = $props();
+	let { id, title, body, username, email, createdAt, status } = $props();
+
+	const resolved = $derived(resolve('/task/[slug]', { slug: id }));
 </script>
 
 <li class="my-3 cursor-pointer p-4 hover:shadow-lg" transition:slide>
-	<div class="flex justify-between">
-		<div>
-			<h3 class="text-xl"><StatusIndicator {status} /> {title}</h3>
-			<p class="text-[12px] text-gray-600">{username ? username : email} text-shadow-lg</p>
+	<a href={resolved}>
+		<div class="flex justify-between">
+			<div>
+				<h3 class="text-xl"><StatusIndicator {status} /> {title}</h3>
+				<p class="text-[12px] text-gray-600">{username ? username : email} text-shadow-lg</p>
+			</div>
+			<span class="mt-1 text-[10px] text-gray-600">{createdAt.toLocaleString()}</span>
 		</div>
-		<span class="mt-1 text-[10px] text-gray-600">{createdAt.toLocaleString()}</span>
-	</div>
-	<p>{body}</p>
+		<p>{body}</p>
+	</a>
 </li>
