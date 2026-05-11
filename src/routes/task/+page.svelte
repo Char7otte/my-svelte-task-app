e6467626f458dbb16ecb87084aac43c916f82c2c
shar <faethychan@gmail.com>
2026-05-11 12:36:37 +0800

    refactor: extract task form

--- src/routes/task/+page.svelte
+++ src/routes/task/+page.svelte
@@ -1,21 +1,12 @@
 <script lang="ts">
-	import { enhance } from '$app/forms';
-	import { resolve } from '$app/paths';
-	import RequiredAsterisk from '$lib/components/requiredAsterisk.svelte';
-	import type { TaskWithUser } from '$lib/types';
-	import type { PageData } from './$types';
-	import TaskCard from './taskCard.svelte';
+  import { resolve } from '$app/paths';
+  import type { TaskWithUser } from '$lib/types';
+  import type { PageData } from './$types';
+  import TaskCard from './taskCard.svelte';
+  import TaskForm from './taskForm.svelte';
 
 	const { data }: { data: PageData } = $props();
 	let disabled = $derived<boolean>(data.user === undefined);
-
-	let titleInput: string = $state<string>('');
-	let bodyInput: string = $state<string>('');
-
-	function handleClearInputs() {
-		titleInput = '';
-		bodyInput = '';
-	}
 
 	let viewOption: string = $state<'all' | 'mine'>('all');
 	let taskList: TaskWithUser[] = $derived.by(() => {
@@ -34,20 +25,7 @@
 	{/if}
 </div>
 
-<form method="POST" class="mb-10" use:enhance action="?/insert">
-	<fieldset {disabled}>
-		<h2 class="text-2xl">Create a task</h2>
-
-		<label for="title" class="block">Title: <RequiredAsterisk /></label>
-		<input type="text" name="title" id="title" bind:value={titleInput} required class="w-full" />
-
-		<label for="body" class="block">Body:</label>
-		<textarea name="body" id="body" bind:value={bodyInput} class="mb-3 w-full" rows="5"> </textarea>
-
-		<button type="submit" class="primary">Submit</button>
-		<button type="button" onclick={handleClearInputs} class="secondary">Clear</button>
-	</fieldset>
-</form>
+<TaskForm {disabled}/>
 
 <h2 class="text-2xl">
 	<input
---
+++ src/routes/task/taskForm.svelte
@@ -0,0 +1,30 @@
+<script lang="ts">
+	import { enhance } from '$app/forms';
+	import RequiredAsterisk from '$lib/components/requiredAsterisk.svelte';
+
+    let { disabled } = $props();
+
+	let titleInput: string = $state<string>('');
+	let bodyInput: string = $state<string>('');
+
+	function handleClearInputs() {
+		titleInput = '';
+		bodyInput = '';
+	}
+</script>
+
+<form method="POST" class="mb-10" use:enhance action="?/insert">
+	<fieldset {disabled}>
+		<h2 class="text-2xl">Create a task</h2>
+
+		<label for="title" class="block">Title: <RequiredAsterisk /></label>
+		<input type="text" name="title" id="title" bind:value={titleInput} required class="w-full" />
+
+		<label for="body" class="block">Body:</label>
+		<textarea name="body" id="body" bind:value={bodyInput} class="mb-3 w-full" rows="5"> </textarea>
+
+		<button type="submit" class="primary">Submit</button>
+		<button type="button" onclick={handleClearInputs} class="secondary">Clear</button>
+	</fieldset>
+</form>
+
