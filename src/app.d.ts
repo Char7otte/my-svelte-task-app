3159c431baa1dc1f45eff3a53a31c6be4f3659d1
shar <faethychan@gmail.com>
2026-05-11 12:28:55 +0800

    Update app.d.ts

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,13 +1,20 @@
 // See https://svelte.dev/docs/kit/types#app.d.ts
+
+import type { User } from "$lib/types";
+
 // for information about these interfaces
 declare global {
 	namespace App {
 		// interface Error {}
-		// interface Locals {}
+		interface Locals {
+			tasks: TaskWithUser
+			user: User
+		}
 		// interface PageData {}
 		// interface PageState {}
 		// interface Platform {}
 	}
 }
 
-export {};
+export { };
+
